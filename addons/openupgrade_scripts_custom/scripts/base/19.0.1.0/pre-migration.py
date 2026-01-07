1610cd552d83e980ec4e131340e0c9f9d8a6fb17
Chris Busillo <info@shinycomputers.com>
2026-01-07 02:50:15 -0500

    upgrade: normalize legacy user group fields

--- addons/openupgrade_scripts_custom/scripts/base/19.0.1.0/pre-migration.py
+++ addons/openupgrade_scripts_custom/scripts/base/19.0.1.0/pre-migration.py
@@ -1,6 +1,8 @@
 # Copyright 2026 Shiny Computers
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
+import re
+
 from odoo.modules.module import get_module_path
 from openupgradelib import openupgrade
 
@@ -33,8 +35,39 @@ def _mark_missing_manifest_modules_uninstalled(env) -> None:
         )
 
 
+def _clean_user_groups_view(env) -> None:
+    """Normalize legacy user group fields before the 19.0 view update."""
+
+    view_record = env.ref("base.user_groups_view", raise_if_not_found=False)
+    if not view_record:
+        return
+    view_arch = view_record.arch_db or ""
+    if "groups_id" not in view_arch and "sel_groups_" not in view_arch and "in_group_" not in view_arch:
+        return
+    updated_arch = view_arch.replace('name="groups_id"', 'name="group_ids"')
+    updated_arch = updated_arch.replace("name='groups_id'", "name='group_ids'")
+    updated_arch = re.sub(r"<field[^>]+name=\"(sel_groups_|in_group_)[^\"]+\"[^>]*/>", "", updated_arch)
+    updated_arch = re.sub(r"<field[^>]+name='(sel_groups_|in_group_)[^']+'[^>]*/>", "", updated_arch)
+    updated_arch = re.sub(
+        r"<field[^>]+name=\"(sel_groups_|in_group_)[^\"]+\"[^>]*>\s*</field>",
+        "",
+        updated_arch,
+        flags=re.DOTALL,
+    )
+    updated_arch = re.sub(
+        r"<field[^>]+name='(sel_groups_|in_group_)[^']+'[^>]*>\s*</field>",
+        "",
+        updated_arch,
+        flags=re.DOTALL,
+    )
+    if updated_arch == view_arch:
+        return
+    view_record.write({"arch_db": updated_arch})
+
+
 @openupgrade.migrate()
 def migrate(env, version):
     """Pre-migration hook for base (19.0.1.0)."""
 
     _mark_missing_manifest_modules_uninstalled(env)
+    _clean_user_groups_view(env)

7d872947cc59fccc2677c20a900bd5ae0723be08
Chris Busillo <info@shinycomputers.com>
2026-01-09 18:56:08 -0500

    Add repair order calendar display info

--- addons/product_connect/models/repair_order.py
+++ addons/product_connect/models/repair_order.py
@@ -9,11 +9,19 @@ class RepairOrder(models.Model):
     product_list_price = fields.Float(related="product_id.product_tmpl_id.list_price", readonly=False, string="Product Price")
     product_standard_price = fields.Float(related="product_id.product_tmpl_id.standard_price", string="Product Cost")
     repair_cost = fields.Float(compute="_compute_total_estimated_cost", store=True)
+    calendar_display_name = fields.Char(compute="_compute_calendar_display_name", export_string_translation=False)
+    estimated_duration = fields.Float(default=2.0, required=True, help="Estimated repair time in hours.")
 
     @api.depends("move_ids.quantity", "move_ids.product_id.product_tmpl_id.standard_price")
     def _compute_total_estimated_cost(self) -> None:
         for order in self:
             order.repair_cost = sum(m.product_id.product_tmpl_id.standard_price * m.product_uom_qty for m in order.move_ids)
+
+    @api.depends("name", "product_id", "user_id")
+    def _compute_calendar_display_name(self) -> None:
+        for order in self:
+            display_name_parts = (order.name, order.user_id.display_name, order.product_id.display_name)
+            order.calendar_display_name = " - ".join(part for part in display_name_parts if part)
 
     def action_repair_done(self) -> "odoo.values.repair_order":
         res = super().action_repair_done()

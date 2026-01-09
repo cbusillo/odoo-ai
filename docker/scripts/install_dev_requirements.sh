ac21f9ea0b19d788728eabf830d26d205b187149
Chris Busillo <info@shinycomputers.com>
2026-01-08 22:49:41 -0500

    fix: mount restore ssh and skip OpenUpgrade deps

--- docker/scripts/install_dev_requirements.sh
+++ docker/scripts/install_dev_requirements.sh
@@ -42,7 +42,7 @@ install_addon_dev_requirements() {
   cd "$base_dir"
   for addon in */ ; do
     case "$addon" in
-      openupgrade_framework/|openupgrade_scripts/)
+      OpenUpgrade/|openupgrade_framework/|openupgrade_scripts/)
         continue
         ;;
     esac

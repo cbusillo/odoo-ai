8a9c2f11631f4d5e954cb554cbb9999107415e7a
Chris Busillo <info@shinycomputers.com>
2026-01-10 23:15:46 -0500

    ==Remove unused imports and reorganize test module imports

--- addons/product_connect/tests/unit/__init__.py
+++ addons/product_connect/tests/unit/__init__.py
@@ -1,21 +1,23 @@
 # Import all unit test modules
-from . import test_autopost_bills_migration
-from . import test_basic
-from . import test_infrastructure_check
-from . import test_model_motor_product_template
-from . import test_model_motor_workflow
-from . import test_model_motor
-from . import test_model_product_template
-from . import test_model_transaction_mixin
-from . import test_multigraph_python
-from . import test_multigraph_quick_integration
-from . import test_multigraph_view
-from . import test_product_factory
-from . import test_service_product_deleter
-from . import test_service_shopify_helpers
-from . import test_service_shopify_sync
-from . import test_simple_unit
-from . import test_tour_coverage
-
 # Import JavaScript unit tests
+# noinspection PyUnusedImports
 from ..js import js_unit_tests
+from . import (
+    test_autopost_bills_migration,
+    test_basic,
+    test_infrastructure_check,
+    test_model_motor,
+    test_model_motor_product_template,
+    test_model_motor_workflow,
+    test_model_product_template,
+    test_model_transaction_mixin,
+    test_multigraph_python,
+    test_multigraph_quick_integration,
+    test_multigraph_view,
+    test_product_factory,
+    test_service_product_deleter,
+    test_service_shopify_helpers,
+    test_service_shopify_sync,
+    test_simple_unit,
+    test_tour_coverage,
+)

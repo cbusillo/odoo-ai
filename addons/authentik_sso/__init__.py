d93079b13dd14f185d97953888447498d5e0c1dd
Chris Busillo <info@shinycomputers.com>
2026-01-14 14:46:28 -0500

    Fix Authentik post-init hook signature

--- addons/authentik_sso/__init__.py
+++ addons/authentik_sso/__init__.py
@@ -3,8 +3,17 @@ from odoo import SUPERUSER_ID, api
 from . import models
 
 
-def post_init_hook(cr, registry) -> None:
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(cr_or_env, registry=None) -> None:
+    if registry is None:
+        env = cr_or_env
+        if not hasattr(env, "registry"):
+            return
+        if "authentik.sso.config" in env.registry:
+            env["authentik.sso.config"].sudo().apply_from_env()
+            env.cr.commit()
+        return
+
+    env = api.Environment(cr_or_env, SUPERUSER_ID, {})
     if "authentik.sso.config" in env.registry:
         env["authentik.sso.config"].sudo().apply_from_env()
-        cr.commit()
+        cr_or_env.commit()
